f13c99bd96ade28913c3dbdf133b88c59335021a
DevSec <arma.devsec@gmail.com>
2017-07-01 02:14:33 +0200

    + farming cfg

---
+++ Config/Plants.h
@@ -0,0 +1,43 @@
+class Bushes
+{
+    class b_vitis_vinifera_f
+    {
+        displayname = "Grape Vine";
+    };
+    class b_ficusc1s_f
+    {
+        displayname = "Fig Bush";
+    };
+    class b_neriumo2d_f
+    {
+        displayname = "Oleander";
+    };
+    class b_neriumo2s_white_f
+    {
+        displayname = "Oleander White";
+    };
+    class b_opuntiaficusindica3s_f
+    {
+        displayname = "Cactus";
+    };
+};
+
+class Trees
+{
+    class t_oleae1s_f
+    {
+        "Olive Tree"
+    };
+    class t_ficusb1s_f
+    {
+        "Fig Tree"
+    };
+    class t_ficusb2s_f
+    {
+        "Palm Tree"
+    };
+    class t_pinusp3s_f
+    {
+        "Pine Tree"
+    };
+};
