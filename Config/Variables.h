c8bf2269265a5076f3a71da71bc3a728c50b23b3
DevSec <arma.devsec@gmail.com>
2017-07-02 22:43:01 +0200

    + gear default

--- Config/Variables.h
+++ Config/Variables.h
@@ -4,8 +4,12 @@ class Sync
     {
         val = 35000;
     };
+    class Gear
+    {
+        val[] = {"C_man_polo_1_F", "C_man_polo_2_F", "C_man_polo_4_F", "C_man_polo_5_F", "C_man_p_fugitive_F"};
+    };
 };
 class Async
 {
-    
+
 };
